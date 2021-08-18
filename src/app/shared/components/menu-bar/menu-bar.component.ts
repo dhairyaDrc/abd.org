635136c8c6c35708614cbd04a2469e358d36842c
Dhairya Shelar <dhairya.shelar@drcsystems.in>
2021-08-18 15:50:27 +0530

    -Buttons done

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// import buttonJsonData from "src/app/constants/button.json";
+import chapter1JsonData from "src/app/constants/chapter1.json";
+// import chapter2CardJsonData from "../constants/chapter2-cards.json"
+import chapter3CardJsonData from "src/app/constants/chapter3.json"
+import chapter4CardJsonData from "src/app/constants/chapter4.json";
+import  centralWestAsia  from "src/app/constants/central-west-asia.json";
+import  eastAsia  from "src/app/constants/east-asia.json";
+import  pacific  from "src/app/constants/pacific.json";
+import  southasia  from "src/app/constants/south-asia.json";
+import  southeastasia  from "src/app/constants/south-east-asia.json";
+
 
 @Component({
   selector: 'app-menu-bar',
@@ -8,8 +20,11 @@ import { Router } from '@angular/router';
 })
 export class MenuBarComponent implements OnInit {
 
+  data = [];
+
   constructor(
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   isShowMenu = false;
@@ -40,6 +55,35 @@ export class MenuBarComponent implements OnInit {
 
   goToReport(){
     this.router.navigate(['/report']);
+
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params.id);
+
+        if (params.id == 1) {
+          this.data=chapter1JsonData;
+          console.log("this.data",this.data);
+          
+        }else if (params.id == 4) {
+          this.data=chapter4CardJsonData;
+        }else if (params.id == 3) {
+          this.data=chapter3CardJsonData;
+        }else if (params.id == 2){
+          this.router.navigate(['/chapter2']);
+        } else if (params.id==10){
+          this.data=centralWestAsia;
+        }else if (params.id==11){
+          this.data=eastAsia;
+        }else if (params.id==12){
+          this.data=pacific;
+        }else if (params.id==13){
+          this.data=southasia;
+        }else if (params.id==14){
+          this.data=southeastasia;
+        }
+
+      }
+    );
   }
 
   goToAppendixes(){
